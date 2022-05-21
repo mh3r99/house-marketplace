257b24f0870ecc74cf4c2d997143c43067eaab63
Mher Hovhannisyan <mh3r99@gmail.com>
2022-05-22 00:14:04 +0400

    create listing page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Category from "./pages/Category";
+import CreatingListing from "./pages/CreatingListing";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<CreatingListing />} />
         </Routes>
         <Navbar />
       </BrowserRouter>
