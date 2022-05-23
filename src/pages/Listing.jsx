b42d770c0a4984237de0936d5a6047d1f15f46d2
Mher Hovhannisyan <mh3r99@gmail.com>
2022-05-23 21:58:28 +0400

    leaflet map

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -5,11 +5,14 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { db } from "../firebase.config";
 import shareIcon from "../assets/svg/shareIcon.svg";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 function Listing() {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
   const [shareLinkCopied, setShareLinkCopied] = useState(false);
+
+  console.log(listing);
 
   const navigate = useNavigate();
   const params = useParams();
@@ -21,7 +24,6 @@ function Listing() {
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
-        console.log(docSnap.data());
         setListing(docSnap.data());
         setLoading(false);
       }
@@ -71,7 +73,6 @@ function Listing() {
             ${listing.regularPrice - listing.discountedPrice} discount
           </p>
         )}
-
         <ul className="listingDetailsList">
           <li>
             {listing.bedrooms > 1
@@ -87,8 +88,25 @@ function Listing() {
           <li>{listing.furnished && "Furnished"}</li>
         </ul>
         <p className="listingLocationTitle">Location</p>
+        <div className="leafletContainer">
+          <MapContainer
+            style={{ height: "100%", width: "100%" }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+            />
 
-        {/* MAP */}
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
@@ -104,3 +122,5 @@ function Listing() {
 }
 
 export default Listing;
+
+// https://stackoverflow.com/questions/67552020/how-to-fix-error-failed-to-compile-node-modules-react-leaflet-core-esm-pat
