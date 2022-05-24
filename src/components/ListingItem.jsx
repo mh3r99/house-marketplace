370eef3000fe9879ef778ade07e7c5b667aca5a7
Mher Hovhannisyan <mh3r99@gmail.com>
2022-05-24 23:28:07 +0400

    edit listing

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg";
+import { ReactComponent as EditIcon } from "../assets/svg/editIcon.svg";
 import bedIcon from "../assets/svg/bedIcon.svg";
 import bathtubIcon from "../assets/svg/bathtubIcon.svg";
 
-function ListingItem({ listing, id, onDelete }) {
+function ListingItem({ listing, id, onEdit, onDelete }) {
   return (
     <li className="categoryListing">
       <Link
@@ -50,8 +51,11 @@ function ListingItem({ listing, id, onDelete }) {
         <DeleteIcon
           className="removeIcon"
           fill="rgb(231,76,60)"
-          onClick={() => onDelete(listing.id, listing.name)}
+          onClick={() => onDelete(listing.id)}
         />
+      )}
+      {onEdit && (
+        <EditIcon className="editIcon" onClick={() => onEdit(listing.id)} />
       )}
     </li>
   );
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -103,6 +103,8 @@ function Profile() {
     }
   };
 
+  const onEdit = (listingId) => navigate(`/edit-listing/${listingId}`);
+
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -161,6 +163,7 @@ function Profile() {
                   listing={listing.data}
                   id={listing.id}
                   onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
